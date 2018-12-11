874cfc8705cc074b927fd7ccdebfb9ee3b340c3f
Raevan Gregson <regregson@fullsail.edu>
2018-12-11 10:28:05 -0500

    webpack configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
-const path = require("path");
-const webpack = require("webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: "./src/index.js",
-  mode: "development",
-  module: {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    port: 8000,
+    publicPath: "http://localhost:8000/dist/",
+  },
+   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
         options: { presets: ["@babel/env"] }
-      },
-      {
+      },      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       }
     ]
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
     filename: "bundle.js"
   },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
 };
